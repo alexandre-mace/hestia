a581ab308827da32684e369277894fe3379d8f51
alexandre-mace <contact.alexandremace@gmail.com>
2018-11-18 15:56:02 +0100

    codacy corrections

--- public/js/choose_type.js
+++ public/js/choose_type.js
@@ -1,6 +1,5 @@
 $(function () {
     $(".site-type").click(function() {
-        console.log($(this));
         const input = $('input[name="choice"]');
         $('input[name="site_type[choice]"]').val($(this).data("rel"));
         $( "form" ).submit();
